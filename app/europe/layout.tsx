fe3182d4f77459c8e39111b2d441d39ddc8bf901
vander1484 <vander14@msn.com>
2026-08-24 10:11:36 +0100

    Activate live Europe campaign map controller

--- app/europe/layout.tsx
+++ app/europe/layout.tsx
@@ -1,5 +1,6 @@
 import "./map-fix.css";
+import MapRuntimeFix from "./MapRuntimeFix";
 
 export default function EuropeLayout({children}:{children:React.ReactNode}){
-  return children;
+  return <>{children}<MapRuntimeFix/></>;
 }
