cc5a1b4da1a92acc7afa44a935b8173eaf5d7b23
vander1484 <vander14@msn.com>
2026-08-25 22:35:51 +0100

    Add corrected Career Path route

---
+++ app/interactive/career-path/page.tsx
@@ -0,0 +1,3 @@
+import "../career-parth/career-parth.css";
+import CareerParthGame from "../career-parth/CareerParthGame";
+export default function CareerPathPage(){return <main className="careerPage"><CareerParthGame/></main>}
