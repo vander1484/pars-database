8dcdb897aa3c145c26af8737f9c828462db54801
vander1484 <vander14@msn.com>
2026-08-30 19:42:03 +0100

    Scope timeline motion to timeline route

---
+++ app/timeline/layout.tsx
@@ -0,0 +1,5 @@
+import TimelineMotion from "../components/TimelineMotion";
+
+export default function TimelineLayout({children}:{children:React.ReactNode}){
+  return <><TimelineMotion/>{children}</>;
+}
