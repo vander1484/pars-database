6027d977f1e03c59e3a8c5071d13b64807b0ad6e
vander1484 <vander14@msn.com>
2026-08-30 18:03:54 +0100

    Enable page transition styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./qa-fixes.css";
 import "./design-upgrade.css";
 import "./nav-animations.css";
 import "./text-reveal.css";
+import "./page-transitions.css";
 import SiteHeader from "./components/SiteHeader";
 import ScrollAnimations from "./components/ScrollAnimations";
 
